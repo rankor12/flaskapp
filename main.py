eeef6ce9a03aae33a13a265616ec2563bf7c475d
rankor12 <86016970+rankor12@users.noreply.github.com>
2024-05-02 19:28:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-from flask import Flask,render_template,request
-import pandas as pd
+from flask import Flask,render_template, request,redirect
+from db import Database
 import pickle
+import pandas as pd
+
+data = pd.read_csv('cleaned data (1).csv')
+pipeline = pickle.load(open('pipeline (2).pkl', 'rb'))
+X = pickle.load(open('df (1).pkl', 'rb'))
 
 app = Flask(__name__)
-data = pd.read_csv('cleaned data.csv')
-pipeline = pickle.load(open('pipeline 1.pkl', 'rb'))
-X = pickle.load(open('df.pkl', 'rb'))
-@app.route("/")
-def index():
+
+dbo = Database()
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/register')
+def about():
+    return render_template('register.html')
+
+@app.route('/perform_registration',methods=['POST'])
+def perform_registration():
+    name = request.form.get('name')
+    telephone = request.form.get('telephone')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    response = dbo.insert(name, telephone, email, password)
+
+    if response:
+        return render_template('login.html', message="register successfully kindly login to proceed")
+    else:
+        return render_template('register.html', message="email already exist")
+
+@app.route('/perform_login',methods=['POST'])
+def perform_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    response = dbo.search(email, password)
+
+    if response:
+        return redirect('/profile')
+    else:
+        return render_template('login.html',message1='incorrect email/password')
+
+
+@app.route('/profile')
+def profile():
     sex = sorted(data['sex'].unique())
     education = sorted(data['education'].unique())
     occupation = sorted(data['occupation'].unique())
@@ -26,17 +65,17 @@ def index():
     lc_brainfog_four = sorted(data['lc_brainfog_four'].unique())
     lc_chestpain_four = sorted(data['lc_chestpain_four'].unique())
     lc_fever_four = sorted(data['lc_fever_four'].unique())
-    return render_template('index.html', sex=sex, education=education, occupation=occupation, diabetes=diabetes,
-                           hypertension=hypertension, asthma=asthma, tb=tb, cancer=cancer, covid_severity=covid_severity, lc_fatigue_four=lc_fatigue_four,
-                           lc_cough_four=lc_cough_four, lc_headache_four=lc_headache_four, lc_breathing_four=lc_breathing_four, lc_taste_four=lc_taste_four,
-                           lc_smell_four=lc_smell_four, lc_brainfog_four=lc_brainfog_four, lc_chestpain_four=lc_chestpain_four, lc_fever_four=lc_fever_four
+    return render_template('profile.html', sex=sex, education=education, occupation=occupation, diabetes=diabetes,
+                           hypertension=hypertension, asthma=asthma, tb=tb, cancer=cancer,
+                           covid_severity=covid_severity, lc_fatigue_four=lc_fatigue_four,
+                           lc_cough_four=lc_cough_four, lc_headache_four=lc_headache_four,
+                           lc_breathing_four=lc_breathing_four, lc_taste_four=lc_taste_four,
+                           lc_smell_four=lc_smell_four, lc_brainfog_four=lc_brainfog_four,
+                           lc_chestpain_four=lc_chestpain_four, lc_fever_four=lc_fever_four
                            )
 
 
-
 @app.route('/predict', methods=['POST'])
-
-
 def predict():
     age = request.form.get('age')
     sex = request.form.get('sex')
@@ -59,11 +98,16 @@ def predict():
     lc_brainfog_four = request.form.get('lc_brainfog_four')
     lc_chestpain_four = request.form.get('lc_chestpain_four')
     lc_fever_four = request.form.get('lc_fever_four')
-    print(age, sex, education, occupation, height, weight, diabetes, hypertension, asthma, tb, cancer, covid_severity, lc_fatigue_four,
-          lc_cough_four, lc_headache_four, lc_breathing_four, lc_taste_four, lc_smell_four, lc_brainfog_four, lc_chestpain_four, lc_fever_four)
+    print(age, sex, education, occupation, height, weight, diabetes, hypertension, asthma, tb, cancer, covid_severity,
+          lc_fatigue_four,
+          lc_cough_four, lc_headache_four, lc_breathing_four, lc_taste_four, lc_smell_four, lc_brainfog_four,
+          lc_chestpain_four, lc_fever_four)
 
-    data = [[age, sex, education, occupation, height, weight, diabetes, hypertension, asthma, tb, cancer, covid_severity, lc_fatigue_four,
-          lc_cough_four, lc_headache_four, lc_breathing_four, lc_taste_four, lc_smell_four, lc_brainfog_four, lc_chestpain_four, lc_fever_four]]
+    data = [
+        [age, sex, education, occupation, height, weight, diabetes, hypertension, asthma, tb, cancer, covid_severity,
+         lc_fatigue_four,
+         lc_cough_four, lc_headache_four, lc_breathing_four, lc_taste_four, lc_smell_four, lc_brainfog_four,
+         lc_chestpain_four, lc_fever_four]]
 
     columns = ['age', 'sex', 'education', 'occupation', 'height', 'weight', 'diabetes',
                'hypertension', 'asthma', 'tb', 'cancer', 'covid_severity',
@@ -84,6 +128,7 @@ def predict():
         covid = "YES - Severe"
 
     return covid
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
